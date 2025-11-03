a4b828865fed7a7c3a0fea52aa772ffb5bc09cba
AryaDK153 <arya.daniswara@ui.ac.id>
2025-11-04 00:19:39 +0700

    Update Item datatype

--- src/Data/Types/Item.hs
+++ src/Data/Types/Item.hs
@@ -1,8 +1,10 @@
 -- focuses on defining data types for items
-module Data.Types.Item where
+module Data.Types.Item (
+    Item(..)
+) where
 
 data Item = Item {
     itemID :: Int,
     itemName :: String,
     itemQty :: Int
-}
+} deriving (Show, Eq)
